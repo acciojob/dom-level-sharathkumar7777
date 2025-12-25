b7d18f9a1286364b261199b1df5aaa1581bd8e75
Sharath Naik <117922641+sharathkumar7777@users.noreply.github.com>
2025-12-25 15:37:25 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
 //your JS code here. If required.
+document.addEventListener("DOMContentLoaded", function () {
+  const element = document.getElementById("level");
+  let level = 0;
+  let current = element;
+
+  while (current) {
+    if (current.nodeType === 1) {
+      level++;
+    }
+    current = current.parentElement;
+  }
+
+  alert(`The level of the element is: ${level}`);
+});
